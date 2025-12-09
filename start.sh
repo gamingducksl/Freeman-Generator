81fd2dc90a737e2b03eafaa415e84ae9d153c57c
Epsilon <mysteriousduckperson@gmail.com>
2025-12-09 00:44:50 +0000

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e  # stop on any error
 
+# Always switch to the script’s directory
+cd "$(dirname "$0")"
+
+# Activate the venv
+source .venv/bin/activate
+
 # --- RUN THE MAIN PROGRAM ---
 echo "Running program..."
 python3 -u freemangenerator1.py | tee logs.txt
